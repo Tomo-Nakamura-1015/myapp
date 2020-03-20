88c434d8d436575e2949b18de3b910d400f9fb6e
github user name <tomodoranicof@gmail.com>
2020-03-20 20:37:13 +0900

    少し機能追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+   def user_params
+    params.require(:user).permit(:name, :email)
+   end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'top#home'
+  get '/about',  to: 'top#about'
+  get '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
+
 end
