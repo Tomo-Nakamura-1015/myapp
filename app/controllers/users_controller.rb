db1e5f6b0af8a7c02f101d196dafd3912ae85a35
github user name <tomodoranicof@gmail.com>
2020-03-23 21:04:12 +0900

    view,css手直し、アクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :destroy, :update, :index, :show]
 
   def index
     @users = User.all
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name,   presence: true
-  validates :email,  presence: true
-  
+  validates :name,   presence: true, length: { maximum: 50 }
+  validates :email,  presence: true, uniqueness: true
+
 #   セキュアにハッシュ化したパスワードをpassword_digestに保存
   has_secure_password validations: true
 end
